c58254bd2361224d1f77eba9f300967d30f37945
Akshay-Srivatsan <srivatsan.akshay@gmail.com>
2016-04-17 08:27:49 -0700

    changed colors

--- Sports/View Controllers/WebViewController.swift
+++ Sports/View Controllers/WebViewController.swift
@@ -17,7 +17,7 @@ class WebViewController: UIViewController, UIWebViewDelegate {
     var titleHistory: [String] = []
     var whichSport: String?
     
-    let STYLE = "<style>* {font-family: sans-serif}</style>";
+    let STYLE = "<style>* {font-family: sans-serif; font-size: 14pt} body{background-color: #DC6665; color: white} a {color: #20FFFF}</style>";
     
     override func viewDidLoad() {
         super.viewDidLoad()
