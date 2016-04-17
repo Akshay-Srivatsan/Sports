94713fe223121a6acf53f05adaf0e926114b89ee
Aaron Brown <ab341@comcast.net>
2016-04-16 20:45:36 -0700

    added a table view cell

---
+++ Sports/Table View Cells/GameFeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GameFeedTableViewCell.swift
+//  Sports
+//
+//  Created by Aaron Brown on 4/16/16.
+//  Copyright © 2016 Akshay Srivatsan. All rights reserved.
+//
+
+import UIKit
+
+class GameFeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
