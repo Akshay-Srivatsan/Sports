985b2c94d9918541aa9688f0676fa12065529cf1
Akshay-Srivatsan <srivatsan.akshay@gmail.com>
2016-04-16 17:21:46 -0700

    added basic ESPN functions

--- Sports/ESPN.swift
+++ Sports/ESPN.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+class ESPN {
+    func getCurrentJSON(callback: [String:AnyObject]? -> ()) {
+        NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: "http://scores.espn.go.com/sports/scores/feed")!, completionHandler: {(data, response, error) -> Void in
+            if error == nil && data != nil {
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [String:AnyObject]
+                    callback(json)
+                } catch {
+                    callback(nil)
+                }
+            }
+        }).resume()
+    }
+    
+    func getJSONForSport(sport: Int, callback: [AnyObject]? -> ()) {
+        NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: "http://scores.espn.go.com/sports/scores/feed")!, completionHandler: {(data, response, error) -> Void in
+            if error == nil && data != nil {
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [String:AnyObject]
+                    var arr = [AnyObject]()
+                    for event in json["comments"] as! [AnyObject] {
+                        if (event["sportId"] as? Int) == sport {
+                            arr.append(event);
+                        }
+                    }
+                    callback(arr)
+                } catch {
+                    callback(nil)
+                }
+            }
+        }).resume()
+    }
+    
+    func getJSONForEvent(id: String, callback: ([AnyObject]?) -> ()) {
+        NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: "http://scores.espn.go.com/sports/scores/feed")!, completionHandler: {(data, response, error) -> Void in
+            if error == nil && data != nil {
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [String:AnyObject]
+                    var arr = [AnyObject]()
+                    for event in json["comments"] as! [AnyObject] {
+                        if (event["key"] as? String)?.rangeOfString(id) != nil {
+                            arr.append(event);
+                        }
+                    }
+                    callback(arr)
+                } catch {
+                    callback(nil)
+                }
+            }
+        }).resume()
+    }
+}
