7a000505d4808f40fcdd2c443f5740a1d8c5bf43
Akshay-Srivatsan <srivatsan.akshay@gmail.com>
2016-04-16 20:44:11 -0700

    Added function to get list of games

--- Sports/ESPN.swift
+++ Sports/ESPN.swift
@@ -59,4 +59,27 @@ class ESPN {
             }
         }).resume()
     }
+    
+    static func getCurrentGames(sport: String, callback: ([String: String]?) -> ()) {
+        let url = "http://espn.go.com/" + sport + "/bottomline/scores"
+        NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: url)!, completionHandler: {(data, response, error) -> Void in
+            if error == nil && data != nil {
+                let eventURLs = String(data: data!, encoding: NSUTF8StringEncoding)?.componentsSeparatedByString("&" + sport + "_s_left")
+                print("Hello")
+                var retval = [String: String]()
+                for url in eventURLs! {
+                    let parts = url.componentsSeparatedByString("gameId=")
+                    if parts.count < 2 {
+                        continue;
+                    }
+                    let id = parts[1].componentsSeparatedByString("&mlb_s_count")[0]
+                    let namePart = parts[0].componentsSeparatedByString("&mlb_s_right")[0]
+                    let name = namePart.substringFromIndex(namePart.rangeOfString("=")!.endIndex).stringByReplacingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+                    retval.updateValue(name, forKey: id)
+                }
+                callback(retval)
+            }
+        }).resume()
+    }
+
 }
